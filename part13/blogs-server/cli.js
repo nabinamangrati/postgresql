eb5ca745590ebfc1cec1c2a6e61080a500808f86
nabinamangrati <mangratinabina1@gmail.com>
2024-09-07 14:35:30 +0545

    13.7 Centralize the application error handling in middleware

--- part13/blogs-server/cli.js
+++ part13/blogs-server/cli.js
@@ -1,10 +1,25 @@
 const express = require("express");
+require("express-async-errors");
 const app = express();
 const blogRouter = require("./controllers/blogs");
 const { PORT } = require("./util/config");
+
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
 const { connectToDatabase } = require("./util/db");
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+
+  if (err.name === "SequelizeValidationError") {
+    return res.status(400).json({ error: err.message });
+  }
+  if (err.status === 404) {
+    return res.status(404).json({ error: err.message || "Not Found" });
+  }
+
+  res.status(500).json({ error: "Internal Server Error" });
+});
 
 const start = async () => {
   await connectToDatabase();
--- part13/blogs-server/controllers/blogs.js
+++ part13/blogs-server/controllers/blogs.js
@@ -1,48 +1,63 @@
 const app = require("express").Router();
 const { Blog } = require("../models/blog");
+
 const blogFinder = async (req, res, next) => {
-  req.blog = await Blog.findByPk(req.params.id);
-  next();
+  try {
+    req.blog = await Blog.findByPk(req.params.id);
+    if (!req.blog) {
+      return res
+        .status(404)
+        .json({ error: `Blog with ID ${req.params.id} not found` });
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
 };
 
 app.get("/", async (req, res) => {
-  const blogs = await Blog.findAll();
-  res.json(blogs);
+  try {
+    const blogs = await Blog.findAll();
+    res.json(blogs);
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.post("/", async (req, res) => {
-  console.log(req.body);
-  const blog = await Blog.create(req.body);
-  res.json(blog);
+app.post("/", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const blog = await Blog.create(req.body);
+    res.status(201).json(blog);
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.get("/:id", blogFinder, async (req, res) => {
-  // const blog = await Blog.findByPk(req.params.id);
-  if (req.blog) {
+app.get("/:id", blogFinder, async (req, res, next) => {
+  try {
     console.log(JSON.stringify(req.blog, null, 2));
-    res.json(req.blog);
-  } else {
-    res.status(404).end();
+    res.status(200).json(req.blog);
+  } catch (err) {
+    next(err);
   }
 });
 app.delete("/:id", blogFinder, async (req, res) => {
-  // const blog = await Blog.findByPk(req.params.id);
-  if (req.blog) {
+  try {
     console.log(req.blog);
     req.blog.destroy();
     res.status(200).send("blog deleted successfully!");
-  } else {
-    res.status(404).send("Blog not found");
+  } catch (err) {
+    next(err);
   }
 });
 app.put("/:id", blogFinder, async (req, res) => {
-  // const note = await Note.findByPk(req.params.id);
-  if (req.blog) {
+  try {
     req.blog.likes = req.body.likes;
     await req.blog.save();
-    res.json(req.blog);
-  } else {
-    res.status(404).end();
+    res.status(200).json(req.blog);
+  } catch (err) {
+    next(err);
   }
 });
 
