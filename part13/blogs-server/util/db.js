09ad6eae0796b6127be6feff50ef68f57ad0f184
nabinamangrati <mangratinabina1@gmail.com>
2024-09-11 19:43:54 +0545

    13.17  Created a migration to initialize 'users' and 'blogs' tables

--- part13/blogs-server/util/db.js
+++ part13/blogs-server/util/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 const { DB_CONNECTION } = require("./config");
-
+const { Umzug, SequelizeStorage } = require("umzug");
 const sequelize = new Sequelize(DB_CONNECTION, {
   dialectOptions: {
     ssl: {
@@ -10,9 +10,27 @@ const sequelize = new Sequelize(DB_CONNECTION, {
   },
   logging: false,
 });
+
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: "migrations/*.js",
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  });
+
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     console.log("connected to the database");
   } catch (err) {
     console.log("failed to connect to the database");
