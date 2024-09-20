9124990c4d672bb43443c38f700eae27f8cb2540
nabinamangrati <mangratinabina1@gmail.com>
2024-09-20 20:48:14 +0545

    13.22 - Add PUT /api/reading-lists/:id route to update "read" status

--- part13/blogs-server/controllers/reading_lists.js
+++ part13/blogs-server/controllers/reading_lists.js
@@ -1,6 +1,26 @@
 const router = require("express").Router();
 
 const { ReadingList } = require("../models");
+const { tokenExtractor } = require("../util/middleware");
+
+const listFinder = async (req, res, next) => {
+  try {
+    req.reading = await ReadingList.findByPk(req.params.id);
+    if (!req.reading) {
+      return res
+        .status(404)
+        .json({ error: `readinglist with ID ${req.params.id} not found` });
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+router.get("/", async (req, res) => {
+  const lists = await ReadingList.findAll();
+  res.json(lists);
+});
 
 router.post("/", async (req, res, next) => {
   const { userId, blogId } = req.body;
@@ -19,6 +39,22 @@ router.post("/", async (req, res, next) => {
     res.status(201).json(readingList);
   } catch (error) {
     next(error);
+  }
+});
+
+router.put("/:id", tokenExtractor, listFinder, async (req, res, next) => {
+  try {
+    if (req.reading.userId !== req.decodedToken.id) {
+      return res
+        .status(403)
+        .json({ error: "You can only update your own reading list items." });
+    }
+
+    req.reading.read = req.body.read;
+    await req.reading.save();
+    res.status(200).json(req.reading);
+  } catch (err) {
+    next(err);
   }
 });
 
